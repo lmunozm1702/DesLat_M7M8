54fda6554e594702e6d7ed89e8f1518513a270bd
lmunozm1702 <lmunozm1702@gmail.com>
2022-04-21 17:14:23 -0400

    emprendedor 2

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -6,24 +6,31 @@ def is_number(number)
 end
 
 #Validar cantidad de parametros
-if ARGV.length != 3
-    puts "ERROR: Se requieren 3 parametros: precio, usuarios y gastos"
+if ARGV.length != 5
+    puts "ERROR: Se requieren 5 parametros: precio, usuarios totales, usuarios premium, usuarios gratuitos y gastos"
     exit
 end
 
 #validar que sean float
-if !is_number(ARGV[0]) or !is_number(ARGV[1]) or !is_number(ARGV[2])
+if !is_number(ARGV[0]) or !is_number(ARGV[1]) or !is_number(ARGV[2]) or !is_number(ARGV[3]) or !is_number(ARGV[4])
     puts "ERROR: Los parametros deben ser numericos"
     exit
 end
 
-#Asignar parametros
 precio = ARGV[0].to_f
-usuarios = ARGV[1].to_f
-gastos = ARGV[2].to_f
+usr_total = ARGV[1].to_f
+usr_premium = ARGV[2].to_f
+usr_gratis = ARGV[3].to_f
+gastos = ARGV[4].to_f
+
+#validar que cuadren los usuarios con el total
+if usr_total != usr_premium + usr_gratis
+    puts "ERROR: No cuadra la cantidad de usuarios totales vs los usuarios premium y usuarios gratuitos"
+    exit
+end
 
 #Calculo utilidad
-utilidades = precio * usuarios - gastos
+utilidades = precio * 2 * usr_premium - gastos
 
 impuesto = 0
 #Calculo impuesto
