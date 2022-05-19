6d3b90ef0f8abf5aad0bf3a06bd61507b6feea70
lmunozm1702 <53025062+lmunozm1702@users.noreply.github.com>
2022-05-19 17:08:19 -0400

    Create blog.sql

---
+++ M7_DB/desafio_blog/blog.sql
@@ -0,0 +1,11 @@
+/*1. Crear base de datos llamada blog.*/
+DROP DATABASE IF EXISTS "dbBlog";
+
+CREATE DATABASE "dbBlog"
+    WITH
+    OWNER = lmunozm1702
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+/*2. Crear las tablas indicadas de acuerdo al modelo de datos.*/
+/*3. Insertar los siguientes registros.*/
